6b969db3312be6815ee5df3e5c6119fdc477b251
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-10-29 17:29:50 +0530

    :rocket:

---
+++ README.md
@@ -0,0 +1,10 @@
+# Drum Kit
+### A Virtual Drum Kit for your Computer 🥁 
+
+Fork, clone, play with the code and feel free to PR for any issues or new versions of my drumkit! 
+
+ - The Keys to play it are very simple. *(WASD for the 4 toms and JKL for Snare, Bass and Symbal)* 
+ - The goal is to.. **Make Interesting form of Music using only Drums !!**
+ - It is made using HTML, CSS and Javascript. 
+ 
+Made with :heart: by Eshaan Khurana.
---
+++ index.js
@@ -0,0 +1,35 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
